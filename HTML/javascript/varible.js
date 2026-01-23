f506c974cf52e10fc3fdd7776272d06f659521ad
amankushwaha91 <amanrajbth6200@gmail.com>
2026-01-23 15:10:24 +0530

    HTML

---
+++ HTML/javascript/varible.js
@@ -0,0 +1,21 @@
+const uid = "abc 123";
+var fullName = "Aman Raj";
+var email = "amanraj@outlook.com";
+var pass = "132";
+var conf = "132";
+var coursecount = 0;
+var islog = false;
+
+console.log(uid);
+console.log("Full Name is :- " + fullName);
+console.log(email);
+console.log(pass);
+console.log(conf);
+
+console.log(`
+ID : ${uid}
+User : ${fullName}
+Email : ${email}
+Password : ${pass}
+Confirm Password : ${conf}
+`);
